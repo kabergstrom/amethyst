7c452129f62afb0817160f31a0122ed22e8fd401
James Lowry <jlowry@users.noreply.github.com>
2020-10-22 15:00:48 +0200

    Copied source file importer out of atelier's old commit

--- amethyst_assets/src/lib.rs
+++ amethyst_assets/src/lib.rs
@@ -49,7 +49,7 @@ pub mod experimental {
         bundle_new::LoaderBundle,
         loader_new::{create_asset_type, AssetUuid, DefaultLoader, LoadStatus, Loader},
         processor::{ProcessingQueue, ProcessingState},
-        simple_importer::SimpleImporter, // SourceFileImporter},
+        simple_importer::{SimpleImporter, SourceFileImporter},
         storage_new::AssetStorage,
     };
     pub use atelier_loader::asset_uuid;
